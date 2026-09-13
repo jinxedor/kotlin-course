853d140ec8c6ec134bc5ab153fd8247919f47c75
Anton Nosatenko <jinxedor@gmail.com>
2026-09-14 00:44:04 +0200

    Homework lesson 4

---
+++ src/main/kotlin/kotlincourse/lessons/lesson04/homework/Datatypes.kt
@@ -0,0 +1,130 @@
+package kotlincourse.lessons.lesson04.homework
+
+// Первая часть домашней работы
+
+val v2: Long = 98765432123456789L
+
+val v3: Float = 23.45f
+
+val v4: Double = 0.123456789
+
+val v5: String = "Kotlin & Java"
+
+val v6: Boolean = FALSE // Неверный ввод значения false. Использовать такое значение можно только если объявить его отдельно как val FALSE = false
+
+val v7: Char = 'c'
+
+val v8: Int = 500
+
+val v9: Long = 4294967296L
+
+val v10: Float = 18.0f
+
+val v11: Double = -0.001
+
+val v12: String = "OpenAI"
+
+val v13: String = "true"
+
+val v14: List<String> = listOf(3, 14)
+
+val v15: Char = '9'
+
+val v16: Int = 2048
+
+val v17: Long = 10000000000L
+
+val v18: Set<String> = setOf("OpenAI", "Quantum Computing")
+
+val v19: Float = 5.75f
+
+val v20 = `1.414` // Ввод такого значения недопустим из за одинарных скобок.
+
+val v21: String = "Artificial Intelligence"
+
+val v22: Array<Char, String> = arrayOf('x', "A") // В Array<> должен быть один тип данных, тут используются Char и String
+
+val v23: String = "Android Studio"
+
+val v24: Char = '@'
+
+val v25: Int = 1024
+
+val v26: Long = 1234567890123L
+
+val v27: Float = 10.01f
+
+val v28: Double = -273.15
+
+val v29: String = "SpaceX"
+
+val v30: Boolean = FALSE // Неверный ввод значения false. Использовать такое значение можно только если объявить его отдельно как val FALSE = false
+
+val v31: Double = 0.007
+
+val v32 = “🤯” // Ввод такого значения недопустим из за использования эмоджи
+
+val v33: Map<Boolean , Int> = mapOf("true" to 2, "false" to 34)
+
+val v34 = ‘65535’ // Ввод такого значения недопустим из за одинарных скобок
+
+val v35: Long = 72057594037927935L
+
+val v36: Float = 2.71828f
+
+val v37: Double = 101.0101
+
+val v38: String = "Quantum Computing"
+
+val v39: Map<Int , Boolean> = mapOf(2 to "true", 34 to "false")
+
+val v40: Char = 'x'
+
+val v1: Byte = 42
+
+val v41: Int = 314
+
+val v42: Long = 123456789123456789L
+
+val v43: Float = 6.626f
+
+val v44: Boolean = TRUE // Неверный ввод значения true. Использовать такое значение можно только если объявить его отдельно как val TRUE = true
+
+
+// Вторая часть домашней работы.
+
+//Хранить букву, на которую указывает палец медиума во время спиритического сеанса.
+val finger: Char = 'F'
+
+//Хранить количество ложек сахара, которые я кладу в одну чашку чая.
+val spoon: Int = 2
+
+//Хранить список расходов на доставку еды, чтобы ещё раз убедиться, что готовить было дешевле.
+val delivery: Double = 22000.0
+
+//Хранить длину очереди в столовой до миллиардной доли сантиметра.
+val dining: Float = 23.13123223124f
+
+//Хранить факт, закрыт ли баг после того, как его просто переименовали в фичу.
+val bugClosed: Boolean = false
+
+//Хранить количество свистков чайника за день.
+val kettleSing: Byte = 4
+
+//Хранить количество нажатий котом на клавиатуру ноутбука за всё время твоей работы.
+val katWorking: Long = 2318409244894L
+
+//Хранить количество попыток пересчитать звёзды на небе за всю историю человечества.
+val starcount: Long = 1393393939291L
+
+//Хранить массу воздуха в спускающем матрасе после нападения кота в долях грамма.
+val airMass: Double = 1.23123123123
+
+//Хранить словарь «название стартапа → сумму потерь инвесторов».
+val startup: Map<String, Int> = mapOf("Startup" to 223010103)
+
+//Хранить строку «починилось само», чтобы закрывать тикеты без лишних слов.
+val fixed: String = "починилось само"
+
+//Хранить список тем для митингов, которые на самом деле никому не нужны.
+val noOneCares: List<String> = listOf("Sasa", "Lele")
